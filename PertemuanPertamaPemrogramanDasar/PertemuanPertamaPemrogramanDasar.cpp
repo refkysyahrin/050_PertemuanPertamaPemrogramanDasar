fb5b28715699b34446a8fea5d6b8d2a932007e14
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-02-27 18:50:07 +0700

    Add project files.

---
+++ PertemuanPertamaPemrogramanDasar/PertemuanPertamaPemrogramanDasar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    float njejari, nluas;
+
+    cout << "masukan jejari = ";
+
+    cin >> njejari;
+
+    nluas = 3.14 * njejari * njejari;
+
+    cout << "luas lingkaran adalah = " << nluas << endl;
+
+    system("pause");
+}
